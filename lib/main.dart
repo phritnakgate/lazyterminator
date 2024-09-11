4462ee882678100d1944f790fa269b57486a182e
phritnakgate <boss5500216@gmail.com>
2024-09-11 21:30:21 +0700

    chore: update main code to select .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,40 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:lazyterminator/screens/auth_page.dart';
-//import 'package:lazyterminator/screens/detailscreen.dart';
 import 'package:lazyterminator/screens/homescreen.dart';
-//import 'package:lazyterminator/screens/startscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'dart:io' show Platform;
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: '.env');
+
+  // Check if running in a local environment (e.g., .env file exists).
+  bool isLocalEnv = Platform.environment.isEmpty; // Vercel will have non-empty environment variables
+  
+  if (isLocalEnv) {
+    // If in local development, load the .env file
+    await dotenv.load(fileName: '.env');
+  }
+
+  // Load Firebase options either from the system environment (Vercel) or the .env file.
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      appId: dotenv.env['APPID'] ?? '',
-      apiKey: dotenv.env['APIKEY'] ?? '',
-      messagingSenderId: dotenv.env['MESSAGESERVICEID'] ?? '',
-      projectId: dotenv.env['PROJECTID'] ?? '',
-      databaseURL: dotenv.env['DATABASEURL'] ?? '',
+      appId: Platform.environment['APPID'] ?? dotenv.env['APPID'] ?? '',
+      apiKey: Platform.environment['APIKEY'] ?? dotenv.env['APIKEY'] ?? '',
+      messagingSenderId: Platform.environment['MESSAGESERVICEID'] ?? dotenv.env['MESSAGESERVICEID'] ?? '',
+      projectId: Platform.environment['PROJECTID'] ?? dotenv.env['PROJECTID'] ?? '',
+      databaseURL: Platform.environment['DATABASEURL'] ?? dotenv.env['DATABASEURL'] ?? '',
     ),
   );
+
+  // Run the Flutter app
   runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "SukhumvitSet"),
-      title: "Lazy Terminator",
-      home: const AuthPage(),
-      routes: <String, WidgetBuilder>{
-        '/home': (BuildContext context) => const HomeScreen(),
-      }));
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: "SukhumvitSet"),
+    title: "Lazy Terminator",
+    home: const AuthPage(),
+    routes: <String, WidgetBuilder>{
+      '/home': (BuildContext context) => const HomeScreen(),
+    },
+  ));
 }
