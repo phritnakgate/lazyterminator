716fee2ec64b296ceedf4cb6f12169f74879b630
phritnakgate <boss5500216@gmail.com>
2024-09-11 21:56:14 +0700

    chore: change from platform to compile-time constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,26 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:lazyterminator/screens/auth_page.dart';
 import 'package:lazyterminator/screens/homescreen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'dart:io' show Platform;
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Check if running in a local environment (e.g., .env file exists).
-  bool isLocalEnv = Platform.environment.isEmpty; // Vercel will have non-empty environment variables
-  
-  if (isLocalEnv) {
-    // If in local development, load the .env file
-    await dotenv.load(fileName: '.env');
-  }
+  // Use compile-time constants for environment variables, injected via --dart-define
+  const String appId = String.fromEnvironment('APPID', defaultValue: '');
+  const String apiKey = String.fromEnvironment('APIKEY', defaultValue: '');
+  const String messagingSenderId = String.fromEnvironment('MESSAGESERVICEID', defaultValue: '');
+  const String projectId = String.fromEnvironment('PROJECTID', defaultValue: '');
+  const String databaseUrl = String.fromEnvironment('DATABASEURL', defaultValue: '');
 
-  // Load Firebase options either from the system environment (Vercel) or the .env file.
+  // Initialize Firebase with environment variables
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-      appId: Platform.environment['APPID'] ?? dotenv.env['APPID'] ?? '',
-      apiKey: Platform.environment['APIKEY'] ?? dotenv.env['APIKEY'] ?? '',
-      messagingSenderId: Platform.environment['MESSAGESERVICEID'] ?? dotenv.env['MESSAGESERVICEID'] ?? '',
-      projectId: Platform.environment['PROJECTID'] ?? dotenv.env['PROJECTID'] ?? '',
-      databaseURL: Platform.environment['DATABASEURL'] ?? dotenv.env['DATABASEURL'] ?? '',
+    options: const FirebaseOptions(
+      appId: appId,
+      apiKey: apiKey,
+      messagingSenderId: messagingSenderId,
+      projectId: projectId,
+      databaseURL: databaseUrl,
     ),
   );
 
